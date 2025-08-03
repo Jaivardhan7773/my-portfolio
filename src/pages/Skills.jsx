9430557c205d6942e5c959acd440d64aa07d83a5
Jaivardhan7773 <jaivardhansinghrathore17@gmail.com>
2025-08-03 13:44:59 +0530

    added skills section

--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -1,58 +1,187 @@
 import React from "react";
-import { ProgressBar } from "react-bootstrap";
-import { FaReact, FaNodeJs, FaDatabase, FaGitAlt } from "react-icons/fa";
-import { SiMongodb, SiJavascript, SiHtml5, SiCss3 } from "react-icons/si";
+import SpotlightCard from "../components/Spotlightcard/SpotlightCard ";
+const roles = [
+  {
+    title: "Web Developer",
+    image: "https://mern-rajesh-portfolio.web.app/assets/web.png",
+  },
+  {
+    title: "React Developer",
+    image: "https://mern-rajesh-portfolio.web.app/assets/frontend.png",
+  },
+  {
+    title: "Backend Developer",
+    image: "https://mern-rajesh-portfolio.web.app/assets/backend.png",
+  },
+  {
+    title: "Full Stack Developer",
+    image: "https://mern-rajesh-portfolio.web.app/assets/fullStack.png",
+  },
+];
+
+
+const technologies = [
+  {
+    name: "HTML 5",
+    icon: "https://cdn.iconscout.com/icon/free/png-512/free-html5-40-1175193.png?f=webp&w=512",
+  },
+  {
+    name: "CSS 3",
+    icon: "https://cdn.iconscout.com/icon/free/png-512/free-css-131-722685.png?f=webp&w=512",
+  },
+  {
+    name: "JavaScript",
+    icon: "https://cdn.iconscout.com/icon/free/png-512/free-javascript-2752148-2284965.png?f=webp&w=512",
+  },
+  {
+    name: "Tailwind CSS",
+    icon: "https://img.icons8.com/?size=96&id=4PiNHtUJVbLs&format=png",
+  },
+  {
+    name: "React JS",
+    icon: "https://cdn.iconscout.com/icon/free/png-512/free-react-3521666-2945110.png?f=webp&w=512",
+  },
+  {
+    name: "Redux",
+    icon: "https://cdn.iconscout.com/icon/free/png-512/free-redux-283024.png?f=webp&w=512",
+  },
+  {
+    name: "Zustand",
+    icon: "https://miro.medium.com/v2/resize:fit:800/1*MX7sS67n80ViuJlNewAmuA.png",
+  },
+  {
+    name: "React bits",
+    icon: "https://cdn.iconscout.com/icon/free/png-512/free-react-13235229-10918763.png?f=webp&w=512",
+  },
+
+  {
+    name: "Bootstrap 5",
+    icon: "https://cdn.iconscout.com/icon/free/png-512/free-bootstrap-7-1175254.png?f=webp&w=512",
+  },
+  {
+    name: "Node JS",
+    icon: "https://cdn.iconscout.com/icon/free/png-512/free-node-js-1174925.png?f=webp&w=512",
+  },
+  {
+    name: "Express JS",
+    icon: "https://cdn.iconscout.com/icon/premium/png-512-thumb/express-js-11217744-9200748.png?f=webp&w=512",
+  },
+  {
+    name: "MongoDB",
+    icon: "https://cdn.iconscout.com/icon/free/png-512/free-mongodb-5-1175140.png?f=webp&w=512",
+  },
+  {
+    name: "Mongoose",
+    icon: "https://img.icons8.com/?size=96&id=gKfcEStXI1Hm&format=png",
+  },
+  {
+    name: "Git",
+    icon: "https://cdn.iconscout.com/icon/free/png-512/free-git-17-1175218.png?f=webp&w=512",
+  },
+  {
+    name: "GitHup",
+    icon: "https://cdn.iconscout.com/icon/free/png-512/free-github-40-432516.png?f=webp&w=512",
+  },
+  {
+    name: "Socket io",
+    icon: "https://cdn.iconscout.com/icon/free/png-512/free-socket-54-282608.png?f=webp&w=512",
+  },
+  {
+    name: "Npm",
+    icon: "https://cdn.iconscout.com/icon/premium/png-512-thumb/npm-11796944-9632855.png?f=webp&w=512",
+  },
+];
+
+const tools = [
+  {
+    name: "VS Code",
+    icon: "https://cdn.iconscout.com/icon/free/png-512/free-vscode-13235441-10918879.png?f=webp&w=512",
+  },
+  {
+    name: "Rest API",
+    icon: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/fastapi/fastapi-original.svg",
+  },
+  {
+    name: "Vercel",
+    icon: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/vercel/vercel-original.svg",
+  },
+  {
+    name: "Netlify",
+    icon: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/netlify/netlify-original.svg",
+  },
+  {
+    name: "Mongodb Compass",
+    icon: "https://mern-rajesh-portfolio.web.app/assets/tools/mongodbCompass.png",
+  },
+  {
+    name: "Postman",
+    icon: "https://cdn.iconscout.com/icon/free/png-512/free-postman-3521648-2945092.png?f=webp&w=512",
+  },
+  {
+    name: "Cloudinary",
+    icon: "https://miro.medium.com/v2/resize:fit:1000/1*7CrSGB37vYF6kWga4PbJGA.png",
+  },
+];
+
+
+const DeveloperCard = ({ title, image }) => (
+  <div className="skill-col" >
+    <img src={image} alt={title} style={{maxHeight:"50px" , maxWidth:"50px" , margin:"10px"}} />
+    <h4 className="skill-title">{title}</h4>
+  </div>
+);
 
 const Skills = () => {
   return (
-    <div className="container py-5">
-      <h2 className="text-center mb-5">My Skills</h2>
-
-      {/* Programming Languages */}
-      <div className="mb-4">
-        <h4>Programming Languages</h4>
-        <ul className="list-unstyled">
-          <li><SiJavascript /> JavaScript - <ProgressBar now={90} label="90%" /></li>
-          <li><SiHtml5 /> HTML5 - <ProgressBar now={95} label="95%" /></li>
-          <li><SiCss3 /> CSS3 - <ProgressBar now={85} label="85%" /></li>
-        </ul>
+    <>
+      <div className="container" style={{marginTop:"50px" , paddingBottom:"50px"}}>
+        <div className="row" style={{ display: "flex" }}>
+          {roles.map((role, index) => (
+            <div className="col">
+              <SpotlightCard
+                key={index}
+                className="custom-spotlight-card"
+                spotlightColor="rgba(0, 229, 255, 0.2)"
+              >
+                <DeveloperCard title={role.title} image={role.image} />
+              </SpotlightCard>
+            </div>
+          ))}
+        </div>
       </div>
 
-      {/* Frameworks & Libraries */}
-      <div className="mb-4">
-        <h4>Frameworks & Libraries</h4>
-        <ul className="list-unstyled">
-          <li><FaReact /> React - <ProgressBar now={80} label="80%" /></li>
-          <li><FaNodeJs /> Node.js - <ProgressBar now={70} label="70%" /></li>
-        </ul>
-      </div>
 
-      {/* Databases */}
-      <div className="mb-4">
-        <h4>Databases</h4>
-        <ul className="list-unstyled">
-          <li><SiMongodb /> MongoDB - <ProgressBar now={75} label="75%" /></li>
-        </ul>
+<div className="container">
+  <div className="row" style={{ display: "flex", flexWrap: "wrap" }}>
+    {technologies.map((tech, index) => (
+      <div className="col" key={index}>
+        <div className="tech" style={{ textAlign: "center", padding: "10px" }}>
+          <img src={tech.icon} alt={tech.name} style={{ width: "60px", height: "60px" }} />
+          <span className="tech-title" style={{ color: "white", display: "block", marginTop: "5px" }}>
+            {tech.name}
+          </span>
+        </div>
       </div>
+    ))}
+  </div>
+</div>
 
-      {/* Tools & Technologies */}
-      <div className="mb-4">
-        <h4>Tools & Technologies</h4>
-        <ul className="list-unstyled">
-          <li><FaGitAlt /> Git - <ProgressBar now={90} label="90%" /></li>
-        </ul>
-      </div>
 
-      {/* Soft Skills */}
-      <div className="mb-4">
-        <h4>Soft Skills</h4>
-        <ul className="list-unstyled">
-          <li>Teamwork & Collaboration</li>
-          <li>Problem-Solving</li>
-          <li>Communication</li>
-        </ul>
+<div className="container">
+  <div className="row" style={{ display: "flex", flexWrap: "wrap" }}>
+    {tools.map((tool, index) => (
+      <div className="col" key={index}>
+        <div className="tech" style={{ textAlign: "center", padding: "10px" }}>
+          <img src={tool.icon} alt={tool.name} style={{ width: "60px", height: "60px" }} />
+          <span className="tech-title" style={{ color: "white", display: "block", marginTop: "5px" }}>
+            {tool.name}
+          </span>
+        </div>
       </div>
-    </div>
+    ))}
+  </div>
+</div>
+    </>
   );
 };
 
